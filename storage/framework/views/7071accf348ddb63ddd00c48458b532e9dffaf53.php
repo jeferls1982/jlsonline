cb580993eab71cbb5f57bcce9542df4c10e2826e
jeferson <jefer.ls@hotmail.com>
2020-09-08 15:18:03 -0300

    resources

--- storage/framework/views/7071accf348ddb63ddd00c48458b532e9dffaf53.php
+++ storage/framework/views/7071accf348ddb63ddd00c48458b532e9dffaf53.php
@@ -7,6 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
         <meta name="generator" content="Jekyll v4.0.1">
+        <meta name="theme-color" content=" #0C1F50">
 
 
         <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/navbar-fixed/">
--- storage/framework/views/e9aede58342fadc9908468feb0f44320ccce4a60.php
+++ storage/framework/views/e9aede58342fadc9908468feb0f44320ccce4a60.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="theme-color" content="#75b529">
         <meta name="generator" content="Jekyll v4.0.1">
         <title>JLSonline-financeiro</title>
 
